382cd537db9057c7a1db006469f684409470d5f6
pikasama <pikasama@qq.com>
2020-08-03 18:40:00 +0800

    blog: style changes

--- static/notificationFx-cookie.js
+++ static/notificationFx-cookie.js
@@ -91,7 +91,7 @@ setTimeout(function (){
             // defaults to the document.body
             wrapper : document.body,
             // the message
-            message : '<div class="ns-thumb"><img height="64px" width="64px" src="https://cdn.jsdelivr.net/gh/PikaSama/shelter-images@1.3.4/images/icon.png"/></div><div style="width:400px;" class="ns-content"><p>第一次访问博客？来看看博客的&nbsp;<a style="color:#1eb4f0;" href="/help">使用教程</a>&nbsp;叭~&nbsp;移动端点击右下角的功能菜单可以切换到侧边栏&nbsp;(浏览后即可永久关闭此通知)</p></div>',
+            message : '<div class="ns-thumb"><img height="72px" width="72px" src="https://cdn.jsdelivr.net/gh/PikaSama/shelter-images@1.3.4/images/icon.png"/></div><div style="width:300px;" class="ns-content"><p>第一次访问博客？来看看<a style="color:#1eb4f0;" href="https://shelter.beaa.cn/help">使用教程</a>&nbsp;叭~<br />移动端请点击右下角的圆点打开侧边栏“<span class="φbk icon-sidebar"></span>”<br />(浏览后即可永久关闭此通知)</p></div>',
             // layout type: growl|attached|bar|other
             layout : 'other',
             // effects for the specified layout:
