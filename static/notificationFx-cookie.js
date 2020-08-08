1003625d513aa019e65a2e7734b1ad1c0393e879
pikasama <pikasama@qq.com>
2020-08-09 01:57:29 +0800

    blog:css

--- static/notificationFx-cookie.js
+++ static/notificationFx-cookie.js
@@ -1,107 +1,28 @@
-//1秒后加载
 setTimeout(function (){
-    //静态资源的版本
-    var ver = "1.3.7";
-    //插入css的地方
-    var cssPlace = $("head");
-    //声明页面路径变量
-    var lc;
-    //声明cookie变量
-    var ck;
-    //检测cookie(判断是否阅读最新公告)
+    // 新人标识
+    var newv = docCookies.hasItem("newbie");
+    // 页面路径
+    var lc = window.location.pathname;
+    // 已读公告日期
     var ad = docCookies.getItem("announcement_date");
-    //设置公告最新日期
+    // 最新公告日期
     var ld = "2020.8.3";
-    //定义检测帮助页面的函数
-    function findHelp (){
-        //加载recycleHelp函数
-        recycleHelp();
-        if (lc != "/help") {
-            ck = docCookies.hasItem("newbie");
-            checkAnnounce();
-        }
+    // 静态资源版本
+    var ver = "1.3.8";
+    // 插入css的地方
+    var cssPlace = $("head");
+    // 非help页面，如果是新人，显示新人通知
+    if (lc != "/help" && newv == false) {
+        newbientf();
     }
-    //定义循环检测帮助页面的函数
-    function recycleHelp (){
-        lc = window.location.pathname;
-        //如果是帮助页面(链接直连)，直接赋予“已读”状态至cookie
-        if(lc == "/help") {
-            docCookies.setItem("newbie", "0", Infinity, "/", "shelter.beaa.cn", true);
-            //cookie更新，重新赋值
-            ck = docCookies.hasItem("newbie");
-            //间接访问帮助页面，清除计时器
-            clearInterval(itv);
-            //执行一次checkAnnounce函数，为转正的新人显示公告
-            checkAnnounce();
-        }
+    // 非help页面，如果不是新人，调用函数
+    else if (lc != "/help" && newv == true) {
+        checkAnnounce();
     }
-    //执行一次findHelp函数，判断首次连接的页面是否为帮助页面，如果是则写入cookie
-    findHelp();
-    //定义检测是否阅读最新公告的函数
-    //公告设置为非萌新可见
-    function checkAnnounce (){
-        if ((ad == null || ad != ld) && ck == true) {
-            //卸载css，中断并删除新人通知
-            $(".ns-box.ns-other.ns-effect-thumbslider.ns-type-error").remove();
-            $("link#thumbslider").remove();
-            //装载css
-            cssPlace.append('<link id="slide" href="//cdn.jsdelivr.net/gh/PikaSama/shelter-images@' + ver + '/static/ns-style-growl.css" rel="stylesheet">');
-            //2秒后加载，持续10秒
-            setTimeout(function (){
-                // create the notification
-                var announcement = new NotificationFx({
-                // element to which the notification will be appended
-                // defaults to the document.body
-                wrapper : document.body,
-                // the message
-                message : '<p>🔔【公告】——2020.8.3<br />叮咚！博客有文章更新啦~<br />更新列表：<br /><a href="/posts/inside-configuration">[持续更新]Inside主题进阶配置</a><br /><br />快去看看吧~&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="#cofirmedAnnouncement">确认公告</a></p>',
-                // layout type: growl|attached|bar|other
-                layout : 'growl',
-                // effects for the specified layout:
-                // for growl layout: scale|slide|genie|jelly
-                // for attached layout: flip|bouncyflip
-                // for other layout: boxspinner|cornerexpand|loadingcircle|thumbslider
-                // ...
-                effect : 'jelly',
-                // notice, warning, error, success
-                // will add class ns-type-warning, ns-type-error or ns-type-success
-                type : 'error',
-                // if the user doesn´t close the notification then we remove it 
-                // after the following time
-                ttl : 10000,
-                // callbacks
-                onClose : function() { return false; },
-                onOpen : function() { return false; }
-                });
-                announcement.show();
-            },2000);
-            confirmAnnounce();
-        }
-    }
-    //声明锚点变量
-    var maodian;
-    //定义锚点检测的函数
-    function confirmAnnounce (){
-        //每秒检测一次锚点变化
-        var itv2 = setInterval(function (){
-            maodian = window.location.hash;
-            if (maodian == "#cofirmedAnnouncement") {
-                //锚点变化，模拟点击事件，写入cookie
-                $(".ns-box.ns-growl.ns-effect-jelly.ns-type-error.ns-show .ns-close").click();
-                docCookies.setItem("announcement_date", ld, Infinity, "/", "shelter.beaa.cn", true);
-                //清除计时器
-                clearInterval(itv2);
-            }
-        },1000);
-        //12秒后清除计时器
-        setTimeout(function (){
-            clearInterval(itv2);
-        },12000);
-    }
-    //如果是新人，显示通知
-    if (ck == false) {
+    // 新人通知
+    function newbientf (){
+        var thumbslider = $(".ns-box.ns-other.ns-effect-thumbslider.ns-type-error .ns-box-inner");
         cssPlace.append('<link id="thumbslider" href="//cdn.jsdelivr.net/gh/PikaSama/shelter-images@' + ver + '/static/ns-style-other.css" rel="stylesheet">');
-        //4秒后加载，持续10秒
         setTimeout(function (){
             // create the notification
             var notification = new NotificationFx({
@@ -109,7 +30,7 @@ setTimeout(function (){
             // defaults to the document.body
             wrapper : document.body,
             // the message
-            message : '<div class="ns-thumb"><img height="72px" width="72px" src="https://cdn.jsdelivr.net/gh/PikaSama/shelter-images@1.3.4/images/icon.png"/></div><div style="width:300px;" class="ns-content"><p>第一次访问博客？来看看<a style="color:#1eb4f0;" href="https://shelter.beaa.cn/help">使用教程</a>&nbsp;叭~<br />移动端请点击右下角的圆点打开侧边栏“<span class="φbk icon-sidebar"></span>”<br />(浏览后即可永久关闭此通知)</p></div>',
+            message : '<div class="ns-thumb"><img height="72px" width="72px" src="https://cdn.jsdelivr.net/gh/PikaSama/shelter-images@1.3.4/images/icon.png"/></div><div style="width:280px;" class="ns-content"><p>第一次访问博客？来看看<a style="color:#1eb4f0;" href="https://shelter.beaa.cn/help">使用教程</a>&nbsp;叭~<br />移动端请点击右下角的圆点打开侧边栏“<span class="φbk icon-sidebar"></span>”<br />(浏览后即可永久关闭此通知)</p></div>',
             // layout type: growl|attached|bar|other
             layout : 'other',
             // effects for the specified layout:
@@ -129,10 +50,57 @@ setTimeout(function (){
             onOpen : function() { return false; }
             });
             notification.show();
-        },4000);
-        //每秒执行一次recycleHelp函数，检测一次页面路径，如果是帮助页面则赋予“已读”状态至cookie
-        var itv = setInterval(function (){
-            recycleHelp();
+            thumbslider.attr("style","width:380px;");
+        },2000);
+    }
+    // 检查是否满足显示公告的条件
+    function checkAnnounce (){
+        // 无已读公告日期或与最新公告日期不符，且不是新人，显示公告
+        if ((ad == null || ad != ld) && newv == true) {
+            announce();
+        }
+    }
+    // 公告通知
+    function announce (){
+        cssPlace.append('<link id="jelly" href="//cdn.jsdelivr.net/gh/PikaSama/shelter-images@' + ver + '/static/ns-style-growl.css" rel="stylesheet">');
+        setTimeout(function (){
+            // create the notification
+            var announcement = new NotificationFx({
+                // element to which the notification will be appended
+                // defaults to the document.body
+                wrapper : document.body,
+                // the message
+                message : '<p>🔔【公告】——2020.8.3<br />叮咚！博客有文章更新啦~<br />更新列表：<br /><a href="/posts/inside-configuration">[持续更新]Inside主题进阶配置</a><br /><br />快去看看吧~&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a id="confirm" href="' + lc + '#cofirmed">确认公告</a></p>',
+                // layout type: growl|attached|bar|other
+                layout : 'growl',
+                // effects for the specified layout:
+                // for growl layout: scale|slide|genie|jelly
+                // for attached layout: flip|bouncyflip
+                // for other layout: boxspinner|cornerexpand|loadingcircle|thumbslider
+                // ...
+                effect : 'jelly',
+                // notice, warning, error, success
+                // will add class ns-type-warning, ns-type-error or ns-type-success
+                type : 'error',
+                // if the user doesn´t close the notification then we remove it 
+                // after the following time
+                ttl : 10000,
+                // callbacks
+                onClose : function() { return false; },
+                onOpen : function() { return false; }
+            });
+            announcement.show();
+            confirmAnnounce();
         },1000);
+    }
+    // 确认公告按钮
+    function confirmAnnounce (){
+        // 监听按钮点击事件
+        $("a#confirm").click(function (){
+            // 关闭公告通知
+            $(".ns-box.ns-growl.ns-effect-jelly.ns-type-error.ns-show .ns-close").click();
+            // 写入已读日期
+            docCookies.setItem("announcement_date", ld, Infinity, "/", "shelter.beaa.cn", true);
+        });
     }
 },1000);
