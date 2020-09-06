2472c14116a6141226c88e5919e6143b2210ceb9
pikasama <pikasama@qq.com>
2020-09-06 20:42:49 +0800

    test

--- SyncOut.sh
+++ SyncOut.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ..
-cp -r shelter-images/static /storage/emulated/0/Documents/termux/shelter-images/static
+cp -r shelter-images/static /storage/emulated/0/Documents/termux/shelter-images/
 if [ $? -eq 1 ];then
   echo Error.
   else
