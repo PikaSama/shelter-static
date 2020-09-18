3b84585f4b6d2ac5cc10fb1fe218a02a2abc59bd
pikasama <pikasama@qq.com>
2020-09-19 00:59:16 +0800

    test

---
+++ js/official/officialFilesLoader-post.js
@@ -0,0 +1,14 @@
+$(()=>{
+    let i = setInterval(()=>{
+        if(typeof delay != "undefined"){
+            clearInterval(i);
+            loadFiles();
+        }
+    },500);
+    function loadFiles(){
+        $("body").append('<script src="//zorin.beaa.cn/test/clipboard-use.js"></script>');
+        $("body").append('<script src="//zorin.beaa.cn/test/pv-counter-post.js"></script>');
+        $("body").append('<script src="//zorin.beaa.cn/test/customization-wordcount.min.js"></script>');
+        $("body").append('<script src="//zorin.beaa.cn/test/customization-minivaline.min.js"></script>');
+    }
+});
